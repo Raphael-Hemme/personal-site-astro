57ee5bd53ff4af23208d80cb4e95a406eff5ca49
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2026-02-15 17:15:44 +0100

    misc: finish tutorial

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,18 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+  const posts = await getCollection('blog');
   return rss({
     title: 'Raphael Hemme | IO-Garden',
     description: 'Some thoughts, notes, code snippets and ideas',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./posts/*.md')),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.id}`,
+    })),
     customData: `<language>en-us</language>`,
   })
 }
